aa2057dd94e30e6067fbd785a93bff2cdb6eab45
dmh <5150636+dmh@users.noreply.github.com>
2024-05-29 15:15:46 +0300

    [WIP] update card v2

---
+++ theme/modules/unicard-filter.module/fields.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import {
+  group,
+  styleGroup,
+  init,
+  moduleFields as fi
+} from '@resultify/hubspot-fields-js'
+import { cardFields, cardStyleFields } from '../../../fields-js/modules/unicard.js'
+
+init(
+  // cardFields(),
+  styleGroup(
+    // cardStyleFields('style.')
+  )
+)
