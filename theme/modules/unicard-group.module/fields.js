d53a15a1faeb2faa27a78410176ff76c620a8bfc
dmh <5150636+dmh@users.noreply.github.com>
2024-08-13 16:19:51 +0300

    [TASK] update unicard-group module, add grid variations for different columns

--- theme/modules/unicard-group.module/fields.js
+++ theme/modules/unicard-group.module/fields.js
@@ -11,10 +11,52 @@ init(
   group('Cards', 'card_group',
     {
       occurrence: {
-        min: 0,
+        min: 1,
         max: 200,
+        default: 1,
         sorting_label_field: 'card_group.heading'
-      }
+      },
+      default: [
+        {
+          media_type: 'full_width_image',
+          full_width_image_group: {
+            full_width_image_aspect_ratio: '16/9'
+          },
+          icon_group: {
+            icon_type: 'fontawesome',
+            icon_size: 80
+          },
+          lottie_group: {
+            lottie_speed: 1,
+            lottie_mode: 'forward',
+            lottie_loop: true
+          },
+          video_group: {
+            video_type: 'hubspot_video',
+            embed: {
+              source_type: 'oembed'
+            }
+          },
+          heading: {
+            heading_tag: 'h2',
+            heading_text: 'Card 1',
+            heading_icon_position: 'left'
+          },
+          subheading: {
+            heading_tag: 'h3',
+            heading_text: 'Subheading',
+            heading_icon_position: 'left'
+          },
+          accordion_group: {
+            accordion_icon_position: 'left',
+            accordion_border_style: 'border_divider'
+          },
+          form_group: {
+            form_heading_tag: 'h3'
+          },
+          module_components: ['media', 'main_heading', 'richtext', 'buttons']
+        }
+      ]
     },
     unicardFields({
       enabledByDefault: ['media', 'main_heading', 'richtext', 'buttons'],
